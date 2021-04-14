9f907b49118ef36a3b3e7dfca27276c67effea36
Samba Diallo <samba.diallo@id-mac007.idtgv.cnit>
2021-04-14 09:51:56 +0200

    mini-mern-ecommerce-project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+// Components
+import Navbar from "./frontend/components/Navbar";
+import SideDrawer from "./frontend/components/SideDrawer";
+import Backdrop from "./frontend/components/Backdrop";
+
+// Screens
+import HomeScreen from "./frontend/screens/HomeScreen";
+import ProductScreen from "./frontend/screens/ProductScreen";
+import CartScreen from "./frontend/screens/CartScreen";
 
 function App() {
+  const [sideToggle, setSideToggle] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar click={() => setSideToggle(true)} />
+      <SideDrawer show={sideToggle} click={() => setSideToggle(false)} />
+      <Backdrop show={sideToggle} click={() => setSideToggle(false)} />
+      <main className="app">
+        <Switch>
+          <Route exact path="/" component={HomeScreen} />
+          <Route exact path="/product/:id" component={ProductScreen} />
+          <Route exact path="/cart" component={CartScreen} />
+        </Switch>
+      </main>
+    </Router>
   );
 }
 
